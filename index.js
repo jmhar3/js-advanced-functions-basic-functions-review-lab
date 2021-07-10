b7199ed2eb3214a34beb18217e9bbbce244fa8f2
jmhar <jmhar@protonmail.com>
2021-07-10 12:13:40 +1000

    more

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(style="*"){
+    return function (adjective="special") {
+        return `You are ${style}${adjective}${style}!`
+    }
+}
+
+const Calculator={
+    add:function(num1,num2){
+        return num1+num2;
+    },
+    subtract:function(num1,num2){
+        return num1-num2;
+    },
+    multiply:function(num1,num2){
+        return num1*num2;
+    },
+    divide:function(num1,num2){
+        return num1/num2;
+    }
+}
+
+let actionApplyer=function(startInt, funcArray){
+   let a=startInt
+   for (let i=0;i<funcArray.length;i++){
+       a=funcArray[i](a)
+   }
+   return a
+}
